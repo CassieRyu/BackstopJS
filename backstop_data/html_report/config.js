599d1ca58f5763089eb561f0e8e3c2ea3d8169fc
LIU Shan <793399678@qq.com>
2019-04-07 22:28:54 +0800

    Add files via upload

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,137 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_Starbucks_Rostary_0_header-container_0_phone.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_Starbucks_Rostary_0_header-container_0_phone.png",
+        "selector": ".header-container",
+        "fileName": "backstop_default_Starbucks_Rostary_0_header-container_0_phone.png",
+        "label": "Starbucks Rostary",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.starbucksreserve.com/en-us",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "error": "Reference file not found /Users/shanliu/Projects/QA/BackstopJS/backstop_data/image_base_reference/backstop_default_Starbucks_Rostary_0_header-container_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_Starbucks_Rostary_1_footer-main_0_phone.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_Starbucks_Rostary_1_footer-main_0_phone.png",
+        "selector": ".footer-main",
+        "fileName": "backstop_default_Starbucks_Rostary_1_footer-main_0_phone.png",
+        "label": "Starbucks Rostary",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.starbucksreserve.com/en-us",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_Starbucks_Rostary_0_header-container_1_desktop.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_Starbucks_Rostary_0_header-container_1_desktop.png",
+        "selector": ".header-container",
+        "fileName": "backstop_default_Starbucks_Rostary_0_header-container_1_desktop.png",
+        "label": "Starbucks Rostary",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.starbucksreserve.com/en-us",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /Users/shanliu/Projects/QA/BackstopJS/backstop_data/image_base_reference/backstop_default_Starbucks_Rostary_0_header-container_1_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_Starbucks_Rostary_1_footer-main_1_desktop.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_Starbucks_Rostary_1_footer-main_1_desktop.png",
+        "selector": ".footer-main",
+        "fileName": "backstop_default_Starbucks_Rostary_1_footer-main_1_desktop.png",
+        "label": "Starbucks Rostary",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.starbucksreserve.com/en-us",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_BackstopJS_GitHub_Page_0_document_0_phone.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_BackstopJS_GitHub_Page_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_GitHub_Page_0_document_0_phone.png",
+        "label": "BackstopJS GitHub Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://github.com/garris/BackstopJS/issues",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../image_base_reference/backstop_default_BackstopJS_GitHub_Page_0_document_1_desktop.png",
+        "test": "../image_history_test/20190407-221842/backstop_default_BackstopJS_GitHub_Page_0_document_1_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_GitHub_Page_0_document_1_desktop.png",
+        "label": "BackstopJS GitHub Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://github.com/garris/BackstopJS/issues",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 69
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
